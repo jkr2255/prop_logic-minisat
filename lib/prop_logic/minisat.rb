3457ab61637a9afef37c0f9aee0c685e4675a0da
Jkr2255 <magnesium.oxide.play@gmail.com>
2016-01-26 22:19:25 +0900

    initial commit

---
+++ lib/prop_logic/minisat.rb
@@ -0,0 +1,7 @@
+require "prop_logic/minisat/version"
+
+module PropLogic
+  module Minisat
+    # Your code goes here...
+  end
+end
---
+++ lib/prop_logic/minisat/version.rb
@@ -0,0 +1,5 @@
+module PropLogic
+  module Minisat
+    VERSION = "0.1.0"
+  end
+end
